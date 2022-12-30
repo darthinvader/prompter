d22397b7e6d4792a6dba4b13d08bf885f219ed2d
Ioannis <psaradakisj@gmail.com>
2022-12-30 17:20:32 +0200

    feat: Scrape api

--- src/utils/scrape.ts
+++ src/utils/scrape.ts
@@ -4,17 +4,35 @@ const client = new ScrapingAntClient({
   apiKey: "47ea69e05dec4bbc8e5288a3cec3fa02",
 });
 
-export const scrape = () => {
+export const describeWord = (word: string) => {
   client
     .scrape(
-      "https://describingwords.io/api/descriptors?term=sword&sortType=frequency"
+      `https://describingwords.io/api/descriptors?term=sword&sortType=${word}`
     )
-    .then((res) =>
+    .then((res: string) =>
       console.log(
         JSON.parse(
           res.content
             .replace("<html><head></head><body>", "")
             .replace("</body></html>", "")
+        )
+      )
+    )
+    .catch((err) => console.error(err.message));
+};
+
+export const relatedWords = (word: string) => {
+  client
+    .scrape(`https://relatedwords.org/api/related?term=swords${word}`)
+    .then((res: string) =>
+      console.log(
+        JSON.parse(
+          res.content
+            .replace(
+              '<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">',
+              ""
+            )
+            .replace("</pre></body></html>", "")
         )
       )
     )
