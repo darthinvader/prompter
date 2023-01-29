29548a512324d2c25561eaaccdc4f3ed2a00ad3e
Ioannis <psaradakisj@gmail.com>
2023-01-29 22:13:16 +0200

    feat: copy field works

--- src/components/CopyField.tsx
+++ src/components/CopyField.tsx
@@ -9,15 +9,26 @@ const CopyField = () => {
 
   const Descriptions = () =>
     descriptions?.map((value) => (
-      <WordChip>
+      <WordChip onClick={() => navigator.clipboard.writeText(value)}>
         {value} <MdOutlineContentCopy />
       </WordChip>
     ));
 
+  const CopyAllToClipboard = () => {
+    const values = descriptions?.reduce(
+      (accumulator, currentValue) => `${accumulator}${currentValue}, `,
+      ""
+    );
+    navigator.clipboard.writeText(values);
+  };
+
   return (
     <div className="flex justify-between p-8 bg-zinc-900 items-start">
       <div className="flex gap-2 flex-wrap">{Descriptions()}</div>
-      <button className="flex justify-center items-center bg-neutral-700 hover:bg-neutral-600 p-2 active:bg-neutral-500 rounded-md text-white">
+      <button
+        onClick={CopyAllToClipboard}
+        className="flex justify-center items-center bg-neutral-700 hover:bg-neutral-600 p-2 active:bg-neutral-500 rounded-md text-white"
+      >
         Copy All To Clipboard
         <HiClipboardCopy size={70} />
       </button>
