c77507d5c811a3541169b7547906a702598c65fe
Ioannis <psaradakisj@gmail.com>
2023-01-29 18:15:40 +0200

    feat: css WordChip

--- src/components/WordChip.tsx
+++ src/components/WordChip.tsx
@@ -1,15 +1,17 @@
+import { ReactNode } from "react";
+
 export type WordChipProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  label?: string;
+  children?: ReactNode;
 };
 
-const WordChip = ({ onClick, label }: WordChipProps) => {
+const WordChip = ({ onClick, children }: WordChipProps) => {
   return (
     <button
-      className="bg-slate-400  hover:bg-slate-500 py-1 px-4 rounded-full border-2"
+      className="flex justify-center	items-center	gap-x-3	bg-slate-400  hover:bg-slate-500 py-1 px-4 rounded-full border-2 shadow-md"
       onClick={onClick}
     >
-      {label}
+      {children}
     </button>
   );
 };
